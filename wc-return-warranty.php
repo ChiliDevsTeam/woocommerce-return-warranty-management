2b4ff85d8a4e1677dcf4c95ca0ed1037b35e8caf
wpeasysoft <wpeasysoft@gmail.com>
2019-06-01 16:06:25 +0600

    version bump and changelog updated

--- wc-return-warranty.php
+++ wc-return-warranty.php
@@ -3,11 +3,11 @@
 Plugin Name: WooCommerce Return and Warrranty
 Plugin URI: https://wpeasysoft.com/woocommerce-return-and-warranty-management/
 Description: An extension for manage return and warranty system for WooCommerce shop
-Version: 1.0.0
+Version: 1.0.1
 Author: wpeasysoft
 Author URI: https://wpeasysoft.com/
 WC requires at least: 3.0
-WC tested up to: 3.6.1
+WC tested up to: 3.6.4
 Text Domain: wc-return-warranty-management
 Domain Path: /languages/
 License: GPL2
@@ -54,7 +54,7 @@ class WC_Return_Warranty {
      *
      * @var string
      */
-    public $version = '1.0.0';
+    public $version = '1.0.1';
 
     /**
      * Minimum PHP version required
