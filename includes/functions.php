ef3cdf6fc384c0da8dd92e56caf337579c8c80e1
wpeasysoft <wpeasysoft@gmail.com>
2019-06-14 20:01:12 +0600

    fix product warranty data saving issue

--- includes/functions.php
+++ includes/functions.php
@@ -233,11 +233,12 @@ function wcrw_get_warranty_settings( $product_id = 0 ) {
         $override_default = get_post_meta( $product_id, '_wcrw_override_default_warranty', true );
 
         if ( 'yes' == $override_default ) {
-            $product_settings = get_post_meta( $product_id, '_wcrw_product_warranty', true );
-            $settings         = wcrw_transform_warranty_settings( $product_settings );
+            $settings = get_post_meta( $product_id, '_wcrw_product_warranty', true );
+            // $settings         = wcrw_transform_warranty_settings( $product_settings );
             $settings['from'] = 'product';
         } else {
             $admin_settings = get_option( 'wcrw_default_warranty', [] );
+            error_log( print_r( $admin_settings, true ) );
             $settings       = wcrw_transform_warranty_settings( $admin_settings );
         }
 
