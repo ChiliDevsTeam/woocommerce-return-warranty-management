8e148d9099e9841ca832721a1822019cfe60b153
chilidevs <info@chilidevs.com>
2020-10-31 20:50:43 +0600

    chore: change old link to chilidevs

--- templates/admin/get-pro.php
+++ templates/admin/get-pro.php
@@ -16,10 +16,10 @@
 
     <section class="feature-list-item">
         <div class="left-side">
-            <img class="featured" src="https://wpeasysoft.com/wp-content/uploads/2019/10/variable-warranty.gif" alt="Variable product warranty">
+            <img class="featured" src="https://chilidevs.com/wp-content/uploads/2020/10/variable-warranty.gif" alt="Variable product warranty">
         </div>
         <div class="right-side">
-            <img class="abnormal" src="https://wpeasysoft.com/wp-content/uploads/edd/2019/10/variable-type.png" alt="Variable warranty icon">
+            <img class="abnormal" src="https://chilidevs.com/wp-content/uploads/2020/10/variable-type.png" alt="Variable warranty icon">
             <h3>Variable Product Warranty</h3>
             <p>With the pro version, the user can set up warranty options for variance products which will make the products more specific so the customers may choose more easily. In handling the warranty on variance products in the store This option is very beneficiary to the admin as well as to the customers for getting a clear concept on warranty without being confused and this incomparable feature doesn’t come with the free version.</p>
         </div>
@@ -27,21 +27,21 @@
 
     <section class="feature-list-item">
         <div class="left-side">
-            <img src="https://wpeasysoft.com/wp-content/uploads/edd/2019/10/instant-refund.png" alt="Instant and Quick Refund">
+            <img src="https://chilidevs.com/wp-content/uploads/2020/10/instant-refund.png" alt="Instant and Quick Refund">
             <h3>Instant and Quick Product Refund </h3>
             <p>By the subscription to the pro version, the admin can give the refund directly by just one click unlike long tiring procedures of the free version. That’s how the pro version ensures the instant and quick product refund to their customers.</p>
         </div>
         <div class="right-side">
-            <img class="featured" src="https://wpeasysoft.com/wp-content/uploads/2019/10/instant-refund.gif" alt="instant-refund-icon">
+            <img class="featured" src="https://chilidevs.com/wp-content/uploads/2020/10/instant-refund.gif" alt="instant-refund-icon">
         </div>
     </section>
 
     <section class="feature-list-item">
         <div class="left-side">
-            <img class="featured" src="https://wpeasysoft.com/wp-content/uploads/2019/10/store-credit.gif" alt="Refund as a Store credit">
+            <img class="featured" src="https://chilidevs.com/wp-content/uploads/2020/10/store-credit.gif" alt="Refund as a Store credit">
         </div>
         <div class="right-side">
-            <img class="abnormal" src="https://wpeasysoft.com/wp-content/uploads/edd/2019/10/store-credit.png" alt="Store Credit">
+            <img class="abnormal" src="https://chilidevs.com/wp-content/uploads/2020/10/store-credit.png" alt="Store Credit">
             <h3>Refund as a Store credit</h3>
             <p>The pro version has introduced a coupon code system for those products which is not generated cash refund or any replacement. Now Customers can re-use the refund which is transferred to their e-wallet(store credit) to get any desired product from the store but the free version can’t offer this feature.</p>
         </div>
@@ -49,21 +49,21 @@
 
     <section class="feature-list-item">
         <div class="left-side">
-            <img class="abnormal" style="margin-left: -42px;" src="https://wpeasysoft.com/wp-content/uploads/edd/2019/10/conversations.png" alt="Conversation icon">
+            <img class="abnormal" style="margin-left: -42px;" src="https://chilidevs.com/wp-content/uploads/2020/10/conversations.png" alt="Conversation icon">
             <h3>Admin and Customer Conversations</h3>
             <p>The pro version of this plugin comes with the messaging option (which is more instant than email) so the customers and the store owner can be connected a little bit closer and they can get the rapid response of their queries.</p>
         </div>
         <div class="right-side">
-            <img class="featured" src="https://wpeasysoft.com/wp-content/uploads/2019/10/conversations.gif" alt="Conversation between admin and customer">
+            <img class="featured" src="https://chilidevs.com/wp-content/uploads/2020/10/conversations.gif" alt="Conversation between admin and customer">
         </div>
     </section>
 
     <section class="feature-list-item">
         <div class="left-side">
-            <img class="featured" src="https://wpeasysoft.com/wp-content/uploads/2019/10/request-form-builder.gif" alt="Request form builder">
+            <img class="featured" src="https://chilidevs.com/wp-content/uploads/2020/10/request-form-builder.gif" alt="Request form builder">
         </div>
         <div class="right-side">
-            <img class="abnormal" src="https://wpeasysoft.com/wp-content/uploads/edd/2019/10/advance-form-builder.png" alt="Request form builder icon">
+            <img class="abnormal" src="https://chilidevs.com/wp-content/uploads/2020/10/advance-form-builder.png" alt="Request form builder icon">
             <h3>Extra fields for Request Form</h3>
             <p>Some additional fields like Image upload fields, number, multi-check and multi-select fields for extending customer request form. So that customer will be easily describe their problems about retruns</p>
         </div>
